f9e1d56b4357c3469efc2bd0ae2714cf080dc7d5
Kim Wittenburg <codello@wittenburg.kim>
2020-05-25 01:56:00 +0200

    Remove Lucille

--- lucille/views/utils/post_tags.php
+++
@@ -1,8 +0,0 @@
-<div class="lc_post_tags tagcloud">
-	<?php 
-	// $before_tag = '<span class="post_tag">';
-	// $after_tag = '</span>';
-	//the_tags( $before_tag, ' ', $after_tag);  
-	the_tags('', ' ', '');  
-	?>
-</div>
--- lucille/views/utils/search_form_global.php
+++
@@ -1,9 +0,0 @@
-<div id="lc_global_search">
-	<div class="lc_global_search_inner">
-		<?php get_search_form(); ?>
-		
-		<div class="close_search_form">
-			<i class="fa fa-times"></i>
-		</div>
-	</div>
-</div>
