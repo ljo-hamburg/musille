112087ad2c54384f43a8fd2d1e312fb566f08100
Kim Wittenburg <codello@wittenburg.kim>
2020-05-29 18:47:57 +0200

    Rearrange Styles

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -6,6 +6,7 @@ module.exports = {
   rules: {
     "string-quotes": "double",
     "max-nesting-depth": 3,
+    "selector-max-id": 1,
     "selector-max-compound-selectors": 5,
     "selector-no-qualifying-type": null,
     "order/properties-alphabetical-order": null,
