9171ddeeec0dc124b15ea40d3144fb00656d260d
Kim Wittenburg <codello@wittenburg.kim>
2020-06-15 22:49:15 +0200

    Fix Concert Sidebars

--- blocks/concerts.js
+++ blocks/concerts.js
@@ -7,6 +7,7 @@ import { createHigherOrderComponent } from "@wordpress/compose";
 import { select } from "@wordpress/data";
 
 const SKIP_BLOCKS = ["musille/header"];
+const CLASS_NAME = "in-sidebar";
 
 /**
  * Adds a panel to the inspector controls for each root block that contains a toggle
@@ -67,6 +68,30 @@ addFilter(
       type: "boolean",
       default: false,
     };
+    return props;
+  }
+);
+
+/**
+ * Adds a filter that makes sure that the `in-sidebar` attribute is persisted in form of
+ * a class that is added to the block's root element.
+ */
+addFilter(
+  "blocks.getSaveContent.extraProps",
+  "wp-concerts/with-sidebar-toggle",
+  (props, name, { sidebar }) => {
+    if (SKIP_BLOCKS.includes(name)) {
+      return props;
+    }
+    let classes = props.className ? props.className.split(" ") : [];
+    if (sidebar) {
+      if (!classes.includes(CLASS_NAME)) {
+        classes.push(CLASS_NAME);
+      }
+    } else {
+      classes = classes.filter((x) => x !== CLASS_NAME);
+    }
+    props.className = classes.join(" ");
     return props;
   }
 );
